978a07069ad6176b22b6f84baa06f5617b10cdba
wilbrone <wilbroneokoth@gmail.com>
2020-09-20 17:05:03 +0300

    add API query by objects

--- server.js
+++ server.js
@@ -4,22 +4,97 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+    type RandomDie {
+        numSides: Int!
+        rollOnce: Int!
+        roll(numRolls: Int!): [Int]
+    }
+
+    
+    input MessageInput{
+        content: String
+        author: String
+    }
+    
+    type Message{
+        id: ID!
+        content: String
+        author: String
+    }
+    
     type Query{
-        hello: String
+        quoteOfTheDay: String
+        random: Float!
+        getDie(numSides: Int): RandomDie 
+        getMessage(id: ID!): Message
+    }
+
+    type Mutation{
+        createMessage(input: MessageInput): Message
+        updateMessage(id: ID!, input: MassageInput): Message
     }
 `);
 
+class RandomDie {
+    constructor(numSides) {
+        this.numSides = numSides;
+    }
+
+    rollOnce() {
+        return 1 + Math.floor(Math.random() * this.numSides);
+    }
+
+    roll({ numRolls }) {
+        var output = [];
+        for (var i = 0; i < numRolls; i++) {
+            output.push(this.rollOnce());
+        }
+        return output;
+    }
+}
+
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+    constructor(id, { content, author }) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+}
+
+// for the message API
+var fakeDatabase = {};
+
 // The root provides a resolver function for each API endpoint
 var root = {
-    hello: () => {
-        return 'Hello World!'
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
     },
-};
+    random: () => {
+        return Math.random();
+    },
+    getDie: ({ numSides }) => {
+        return new RandomDie(numSides || 6);
+    },
+    setMessage: ({ message }) => {
+        fakeDatabase.message = message;
+        return message;
+    },
 
-// Run the GraphQL query '{ hello }' and print out the response
-// graphql(schema, '{ hello }', root).then((response) => {
-//     console.log(response);
-// })
+    getMessage: ({ id }) => {
+        if (!fakeDatabase[id]) {
+            throw new Error('no message exists with id ' + id);
+        }
+        return new Message(id, fakeDatabase[id]);
+    },
+
+    createMessage: ({ input }) => {
+        // Create a random id for our "database".
+        var id = require('crypto').randomBytes(10).toString("hex");
+        fakeDatabase[id] = id;
+        return new Message(id, input)
+    }
+};
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
@@ -29,3 +104,4 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
