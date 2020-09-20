e710ffe782453d91830716522e77b90cf6438e87
wilbrone <wilbroneokoth@gmail.com>
2020-09-20 15:00:46 +0300

    running express and graphql

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require('graphql');
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -10,11 +12,20 @@ var schema = buildSchema(`
 // The root provides a resolver function for each API endpoint
 var root = {
     hello: () => {
-        return 'Hello World! & reply'
+        return 'Hello World!'
     },
 };
 
 // Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-    console.log(response);
-})
+// graphql(schema, '{ hello }', root).then((response) => {
+//     console.log(response);
+// })
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(4000);
